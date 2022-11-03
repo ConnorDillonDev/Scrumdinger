0f3de0ec65dca11735cfcd4147d3b2272a334f64
Connor Dillon <Connordillon96@gmail.com>
2022-11-03 15:10:41 +0000

    edit view and .page w/ toolbar

--- Scrumdinger/Models/DetailView.swift
+++ Scrumdinger/Models/DetailView.swift
@@ -40,7 +40,6 @@ struct DetailView : View {
             }
             Section(header: Text("Attendees")){
                 ForEach(scrum.attendees) {attendee in Label(attendee.name, systemImage: "person")
-                    
                 }
             }
         }
